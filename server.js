7e3d71a9748590f5db8bd58e7c85de867864a023
mesem corlu <mesemcorlu@gmail.com>
2026-09-18 01:33:38 +0300

    Update DB user default to avnadmin

--- server.js
+++ server.js
@@ -16,13 +16,14 @@ app.get('/', (req, res) => {
 
 app.use(cors());
 app.use(express.json());
+
 // MySQL Bağlantı Havuzu
 const db = mysql.createPool({
-    host: process.env.DB_HOST || 'localhost',
-    port: process.env.DB_PORT || 3306,
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || '',
-    database: process.env.DB_NAME || 'corlu_mem',
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT) || 16272,
+    user: process.env.DB_USER || 'avnadmin',
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME || 'defaultdb',
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
